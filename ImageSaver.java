3d806a17880f85d8a0e8e3ff302af32b7df18764
Gusev Roman <114294990+Ghenntoggy1@users.noreply.github.com>
2023-07-25 14:50:04 +0300

    Added project files

---
+++ ImageSaver.java
@@ -0,0 +1,33 @@
+import edu.duke.DirectoryResource;
+import edu.duke.ImageResource;
+import java.io.File;
+
+public class ImageSaver {
+    public void doSaveGray () {
+        DirectoryResource dr = new DirectoryResource();
+        for (File f : dr.selectedFiles()) {
+            GrayScaleConvertor gray = new GrayScaleConvertor();
+            ImageResource image1 = new ImageResource(f);
+            String fname = image1.getFileName();
+            String newName = "gray-" + fname;
+            image1 = gray.makeGray(image1);
+            image1.setFileName(newName);
+            image1.draw();
+            image1.save();
+        }
+    }
+    public void doSaveInverted () {
+        DirectoryResource dr = new DirectoryResource();
+        for (File f : dr.selectedFiles()) {
+            BatchInversions inverted = new BatchInversions();
+            ImageResource image1 = new ImageResource(f);
+            String fname = image1.getFileName();
+            String newName = "inverted-" + fname;
+            image1 = inverted.makeInversion(image1);
+            image1.setFileName(newName);
+            image1.draw();
+            image1.save();
+            
+        }
+    }
+}
